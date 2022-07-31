2ad4e09a9c47a6f5c2542a5f0178d0497dfea663
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-07-31 16:07:17 +0300

    task1 createMarkup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const imgMarkup = createImgMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createImgMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+        <img class="gallery__image"
+        src="${preview}"
+        alt="${description}">
+        </div>`;
+		})
+		.join("");
+}

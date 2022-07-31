d4432da106f743696dda925fa66c1a13509b319f
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-07-31 20:03:49 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -12,10 +12,29 @@ function createImgMarkup(galleryItems) {
 	return galleryItems
 		.map(({ preview, original, description }) => {
 			return `<div class="gallery__item">
-        <img class="gallery__image"
-        src="${preview}"
-        alt="${description}">
-        </div>`;
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
 		})
 		.join("");
+}
+
+gallery.addEventListener("click", imgClick);
+
+function imgClick(e) {
+	e.preventDefault();
+	if (!e.target.classList.contains("gallery__image")) {
+		return;
+	}
+	const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+
+	instance.show();
 }
